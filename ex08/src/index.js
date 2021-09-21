5866a515f13e9a25816172c09f23a1e9ca35aec6
amelzekovic <amelzekovic@gmail.com>
2021-09-22 00:28:09 +0200

    Else Statements

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,8 +1,8 @@
 function logicalOrOperator(num) {
-    if (num < 20 || num > 30) {
-        return "Out";
-    }
-    return "In";
+  if (num < 20 || num > 30) {
+      return "Out";
+  }
+  return "In";
 }
 
 console.log(logicalOrOperator(0));
